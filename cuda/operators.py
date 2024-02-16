2134c0cdafa860a0c5372de1228296e515b6c807
IgorBaratta <igorbaratta@gmail.com>
2024-02-16 15:12:30 +0000

    update documentation

--- cuda/operators.py
+++ cuda/operators.py
@@ -13,13 +13,31 @@ def mass_operator(
     """
     Compute the mass operator on some entities.
 
+    This kernel computes the mass operator on a set of entities (cells or
+    facets) defined by the entity dofmap. The mass operator is applied to the
+    input array `x` and accumulates the result in the output array `y`.
+
     Parameters
     ----------
-    x : input array
-    entity_constants : array containing the material coefficients
-    y : output array
-    detJ_f : array containing the determinant of the Jacobian on the boundary facets
-    entity_dofmap : 2d array containing the local DOF on given entities
+    x : numba.types.Array
+        Input array.
+    entity_constants : numba.types.Array
+        Array containing the material coefficients associated with each entity.
+    y : numba.types.Array
+        Output array where the result of applying the mass operator is accumulated.
+    detJ_entity : numba.types.Array
+        Array containing the determinant of the Jacobian associated with each entity.
+    entity_dofmap :  numba.types.Array
+        2D array containing the local degrees of freedom (DOF) on given entities.
+
+    Notes
+    -----
+    - The kernel performs atomic addition to accumulate results in the output array
+      `y` to handle potential race conditions arising from concurrent writes.
+    - This kernel should be launched with a sufficient number of threads and blocks
+      to cover all the elements in `entity_dofmap`.
+    - detJ_entity and entity_dofmap are assumed to be 2D arrays with the same shape.
+
     """
     thread_id = cuda.threadIdx.x  # Local thread ID (max: 1024)
     block_id = cuda.blockIdx.x  # Block ID (max: 2147483647)
@@ -29,6 +47,11 @@ def mass_operator(
     local_dof = idx % entity_dofmap.shape[1]
 
     if idx < entity_dofmap.size:
+        # Compute the global DOF index
         dof = entity_dofmap[facet, local_dof]
+
+        # Compute the contribution of the current DOF to the mass operator
         value = x[dof] * detJ_entity[facet, local_dof] * entity_constants[facet]
+
+        # Atomically add the computed value to the output array `y`
         cuda.atomic.add(y, dof, value)
