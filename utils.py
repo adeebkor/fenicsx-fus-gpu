5115be2578d1a15f532b21729c2c918968235fc1
Adeeb Arif Kor <mabm4@cam.ac.uk>
2024-02-22 17:04:45 +0800

    Add functions for eval

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.typing as npt
 from dolfinx.mesh import Mesh
+from dolfinx.geometry import bb_tree, compute_collisions_points, compute_colliding_cells
 
 
 def facet_integration_domain(facets: npt.NDArray[np.int32], mesh: Mesh):
@@ -37,3 +38,43 @@ def facet_integration_domain(facets: npt.NDArray[np.int32], mesh: Mesh):
         boundary_data[i, 1] = local_facet[0][0]
 
     return boundary_data
+
+
+def compute_eval_params(mesh, points, float_type):
+    """
+    Compute the parameters required for dolfinx.Function eval
+
+    Parameters
+    ----------
+
+    mesh : dolfinx.mesh
+
+    points : numpy.ndarray
+            The evaluation points of shape (3 by n) where each row corresponds
+            to x, y, and z coordinates.
+
+    Returns
+    -------
+
+    points_on_proc : numpy.ndarray
+            The evaluation points owned by the process.
+
+    cells : list
+            A list containing the cell index of the evaluation point.
+    """
+
+    tree = bb_tree(mesh, mesh.topology.dim, padding=1e-12)
+    cells = []
+    points_on_proc = []
+    cell_candidates = compute_collisions_points(tree, points.T)
+    cell_collisions = compute_colliding_cells(mesh, cell_candidates, points.T)
+
+    for i, point in enumerate(points.T):
+        # Only use evaluate points on current processor
+        if len(cell_collisions.links(i)) > 0:
+            points_on_proc.append(point)
+            cells.append(cell_collisions.links(i)[0])
+
+    points_on_proc = np.array(points_on_proc, dtype=float_type)
+
+    return points_on_proc, cells
