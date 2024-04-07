99688e73ebf02d27070c2ffab774a37e6045c6e1
Adeeb Arif Kor <mabm4@cam.ac.uk>
2024-04-07 15:04:02 +0800

    Add scatterer implementation and tests

---
+++ numba-cpu/scatterer.py
@@ -0,0 +1,136 @@
+"""
+==========
+Scatterers
+==========
+
+This file contains the kernels for MPI communication, namely the scattering
+operation. It includes the scatter reverse and scatter forward functions.
+
+Copyright (C) 2024 Adeeb Arif Kor
+"""
+
+import numpy as np
+import numpy.typing as npt
+from mpi4py import MPI
+
+
+def scatter_reverse(
+        comm: MPI.Comm, owners_data: list, ghosts_data: list, N: int, 
+        float_type: np.dtype[np.floating]):
+
+    """
+    Outer function to capture the constant variables of the scatter reverse
+    operation.
+
+    Parameters
+    ----------
+    comm : MPI communicator
+    owners_data : degrees-of-freedom data in this process that are owned by 
+        other processes
+    ghosts_data : degrees-of-freedom data that are owned by this process and 
+        are ghosts in other processes 
+    N : number of ghosts in this processes
+    float_type : buffer's floating-point type
+
+    Return
+    ------
+    scatter : function
+    """
+    
+    owners_idx, owners_size, owners_offsets, owners = owners_data
+    ghosts_idx, ghosts_size, ghosts_offsets, ghosts = ghosts_data
+
+    send_buff = np.zeros(np.sum(owners_size), dtype=float_type)
+    recv_buff = np.zeros(np.sum(ghosts_size), dtype=float_type)
+
+    def scatter(buffer: npt.NDArray[np.floating]):
+        """
+        Perform the scatter reverse operation of the buffer array.
+
+        Parameters
+        ----------
+        buffer : arrays to perform scatter reverse
+        """
+
+        all_requests = []
+
+        send_buff[:] = buffer[-N:][owners_idx]
+        for i, dest in enumerate(owners):
+            begin = owners_offsets[i]
+            end = owners_offsets[i + 1]
+            reqs = comm.Isend(send_buff[begin:end], dest=dest)
+            all_requests.append(reqs)
+
+        for i, src in enumerate(ghosts):
+            begin = ghosts_offsets[i]
+            end = ghosts_offsets[i + 1]
+            reqr = comm.Irecv(recv_buff[begin:end], source=src)
+            all_requests.append(reqr)
+
+        MPI.Request.Waitall(all_requests)
+
+        # Unpack
+        buffer[ghosts_idx] += recv_buff
+
+    return scatter
+
+
+def scatter_forward(
+        comm: MPI.Comm, owners_data: list, ghosts_data: list, N: int, 
+        float_type: np.dtype[np.floating]):
+    
+    """
+    Outer function to capture the constant variables of the scatter forward
+    operation.
+
+    Parameters
+    ----------
+    comm : MPI communicator
+    owners_data : degrees-of-freedom data in this process that are owned by 
+        other processes
+    ghosts_data : degrees-of-freedom data that are owned by this process and 
+        are ghosts in other processes 
+    N : number of ghosts in this processes
+    float_type : buffer's floating-point type
+
+    Return
+    ------
+    scatter : function
+    """
+
+    owners_idx, owners_size, owners_offsets, owners = owners_data
+    ghosts_idx, ghosts_size, ghosts_offsets, ghosts = ghosts_data
+
+    send_buff = np.zeros(np.sum(ghosts_size), dtype=float_type)
+    recv_buff = np.zeros(np.sum(owners_size), dtype=float_type)
+
+    def scatter(buffer):
+        """
+        Perform the scatter forward operation of the buffer array.
+
+        Parameters
+        ----------
+        buffer : arrays to perform scatter forward
+        """
+
+        all_requests = []
+
+        send_buff[:] = buffer[ghosts_idx]
+        for i, dest in enumerate(ghosts):
+            begin = ghosts_offsets[i]
+            end = ghosts_offsets[i + 1]
+            reqs = comm.Isend(send_buff[begin:end], dest=dest)
+            all_requests.append(reqs)
+
+        for i, src in enumerate(owners):
+            begin = owners_offsets[i]
+            end = owners_offsets[i + 1]
+            reqr = comm.Irecv(recv_buff[begin:end], source=src)
+            all_requests.append(reqr)
+
+        MPI.Request.Waitall(all_requests)
+
+        # Pack
+        buffer[-N:][owners_idx] = recv_buff
+    
+    return scatter
---
+++ numba-cpu/test_scatterer.py
@@ -0,0 +1,124 @@
+import numpy as np
+from mpi4py import MPI
+
+import basix
+import basix.ufl
+from dolfinx.fem import functionspace, Function
+from dolfinx.la import InsertMode
+from dolfinx.mesh import create_box, CellType, GhostMode
+
+from scatterer import scatter_forward, scatter_reverse
+
+# MPI
+comm = MPI.COMM_WORLD
+rank = comm.rank
+
+# Set float type
+float_type = np.float64
+
+P = 4  # Basis function order
+
+nd = P + 1
+Nd = nd * nd * nd
+
+N = 4
+mesh = create_box(
+  comm, ((0., 0., 0.), (1., 1., 1.)),
+  (N, N, N), cell_type=CellType.hexahedron, 
+  ghost_mode=GhostMode.none,
+  dtype=float_type
+)
+
+# Tensor product element
+family = basix.ElementFamily.P
+variant = basix.LagrangeVariant.gll_warped
+cell_type = mesh.basix_cell()
+
+basix_element = basix.create_element(family, cell_type, P, variant)  # doesn't work with tp element
+element = basix.ufl._BasixElement(basix_element)  # basix ufl element
+
+# Prepare data for scatterer
+V = functionspace(mesh, element)
+imap = V.dofmap.index_map
+
+# Compute ghosts data in this process that are owned by other processes
+nlocal = imap.size_local
+nghost = imap.num_ghosts
+owners = imap.owners
+unique_owners, owners_size = np.unique(owners, return_counts=True)
+owners_idx = np.argsort(owners)
+
+owners_offsets = np.cumsum(owners_size)
+owners_offsets = np.insert(owners_offsets, 0, 0)
+
+# Compute owned data by this process that are ghosts data in other process 
+shared_dofs = imap.index_to_dest_ranks()
+shared_ranks = np.unique(shared_dofs.array)
+
+ghosts = []
+for shared_rank in shared_ranks:
+    for dof in range(nlocal):
+        if shared_rank in shared_dofs.links(dof):
+            ghosts.append(shared_rank)
+
+ghosts = np.array(ghosts)
+unique_ghosts, ghosts_size = np.unique(ghosts, return_counts=True)
+ghosts_offsets = np.cumsum(ghosts_size)
+ghosts_offsets = np.insert(ghosts_offsets, 0, 0)
+
+# Communicate the ghost indices
+all_requests = []
+
+# Send
+send_buff_idx = np.zeros(np.sum(owners_size), dtype=np.int64)
+send_buff_idx[:] = imap.ghosts[owners_idx]
+for i, owner in enumerate(unique_owners):  # send to destination
+    begin = owners_offsets[i]
+    end = owners_offsets[i + 1]
+    reqs = comm.Isend(send_buff_idx[begin:end], dest=owner)
+    all_requests.append(reqs)
+
+# Receive
+recv_buff_idx = np.zeros(np.sum(ghosts_size), dtype=np.int64)
+for i, ghost in enumerate(unique_ghosts):  # receive from source
+    begin = ghosts_offsets[i]
+    end = ghosts_offsets[i + 1]
+    reqr = comm.Irecv(recv_buff_idx[begin:end], source=ghost)
+    all_requests.append(reqr)
+
+MPI.Request.Waitall(all_requests)
+
+ghosts_idx = recv_buff_idx - imap.local_range[0]
+
+owners_data = [owners_idx, owners_size, owners_offsets, unique_owners]
+ghosts_data = [ghosts_idx, ghosts_size, ghosts_offsets, unique_ghosts]
+
+# Define function for testing
+u0 = Function(V, dtype=float_type)
+u0.interpolate(lambda x: 100 * np.sin(2*np.pi*x[0]) * np.cos(3*np.pi*x[1])
+               * np.sin(4*np.pi*x[2]))
+u_ = u0.x.array.copy()
+
+# -------------------- #
+# Test scatter reverse #
+# -------------------- #
+
+scatter_rev = scatter_reverse(
+    comm, owners_data, ghosts_data, nghost, float_type
+)
+
+scatter_rev(u_)
+u0.x.scatter_reverse(InsertMode.add)
+print(f"REVERSE: {rank}: {np.allclose(u0.x.array, u_)}", flush=True)
+
+# -------------------- #
+# Test scatter forward #
+# -------------------- #
+
+scatter_fwd = scatter_forward(
+    comm, owners_data, ghosts_data, nghost, float_type
+)
+
+scatter_fwd(u_)
+u0.x.scatter_forward()
+print(f"FORWARD: {rank}: {np.allclose(u0.x.array, u_)}", flush=True)
