8a4125b28a522b3ae08dd6739b5b8419b986e949
Adeeb Arif Kor <mabm4@cam.ac.uk>
2024-04-09 13:04:29 +0800

    Remove comments

--- numba-cpu/test_scatterer.py
+++ numba-cpu/test_scatterer.py
@@ -31,7 +31,7 @@ family = basix.ElementFamily.P
 variant = basix.LagrangeVariant.gll_warped
 cell_type = mesh.basix_cell()
 
-basix_element = basix.create_tp_element(family, cell_type, P, variant)  # doesn't work with tp element, why?
+basix_element = basix.create_tp_element(family, cell_type, P, variant)
 element = basix.ufl._BasixElement(basix_element)  # basix ufl element
 
 # Prepare data for scatterer
