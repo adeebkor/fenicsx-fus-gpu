ce8a7b665e791dcd1473386ac8e81fa56745ff65
Mohd Adeeb Mohd Arif Kor <adeeb@umhpc.dicc.um.edu.my>
2024-04-20 00:40:27 +0800

    Implement and test scatter reverse

--- cuda/scatterer.py
+++ cuda/scatterer.py
@@ -9,6 +9,16 @@ from mpi4py import MPI
 
 
 @cuda.jit
+def ghosts_dof(in_, out_, nlocal):
+    thread_id = cuda.threadIdx.x
+    block_id = cuda.blockIdx.x
+    idx = thread_id + block_id * cuda.blockDim.x
+
+    if idx < out_.size:
+        out_[idx] = in_[idx + nlocal]
+
+
+@cuda.jit
 def pack(in_, out_, index):
     thread_id = cuda.threadIdx.x
     block_id = cuda.blockIdx.x
@@ -29,7 +39,7 @@ def unpack_rev(in_, out_, index):
 
 
 @cuda.jit
-def unpack_rev(in_, out_, index):
+def unpack_fwd(in_, out_, index):
     thread_id = cuda.threadIdx.x
     block_id = cuda.blockIdx.x
     idx = thread_id + block_id * cuda.blockDim.x
@@ -43,11 +53,13 @@ def scatter_reverse(comm, owners_data, ghosts_data, N, float_type):
     ADD DOC!
     """
 
+    nlocal, nghost = N
     owners_idx, owners_size, owners_offsets, owners = owners_data
     ghosts_idx, ghosts_size, ghosts_offsets, ghosts = ghosts_data
 
-    send_buff = cuda.to_device(np.zeros(np.sum(owners_size), dtype=float_type))
-    recv_buff = cuda.to_device(np.zeros(np.sum(ghosts_size), dtype=float_type))
+    send_buff = [cuda.device_array((owner_size,), dtype=float_type) for owner_size in owners_size]
+    recv_buff = [cuda.device_array((ghost_size,), dtype=float_type) for ghost_size in ghosts_size]
+    ghosts_buff = cuda.device_array((nghost, ), dtype=float_type)
 
     def scatter(buffer):
         """
@@ -56,27 +68,74 @@ def scatter_reverse(comm, owners_data, ghosts_data, N, float_type):
 
         all_requests = []
 
-        # Set the number of threads in a block
+        # Set the number of threads
         threadsperblock = 128
-        numblocks = (owners.size + (threadsperblock - 1)) // threadsperblock
 
-        # Pack code
+        # Ghosts degrees-of-freedom
+        numblocks_ghosts = (nghost + (threadsperblock - 1)) // threadsperblock
+        ghosts_dof[numblocks_ghosts, threadsperblock](buffer, ghosts_buff, nlocal)
 
+        # Pack
+        numblocks_pack = [
+            (owner_size + (threadsperblock - 1)) // threadsperblock
+            for owner_size in owners_size
+        ]
+        for i, sb in enumerate(send_buff):
+            pack[numblocks_pack[i], threadsperblock](ghosts_buff, sb, owners_idx[i])
 
+        # Send
         for i, dest in enumerate(owners):
-            begin = owners_offsets[i]
-            end = owners_offsets[i + 1]
-            reqs = comm.Isend(send_buff[begin:end], dest=dest)
+            reqs = comm.Isend(send_buff[i], dest=dest)
             all_requests.append(reqs)
 
+        # Receive
         for i, src in enumerate(ghosts):
-            begin = ghosts_offsets[i]
-            end = ghosts_offsets[i + 1]
-            reqr = comm.Irecv(recv_buff[begin:end], source=src)
+            reqr = comm.Irecv(recv_buff[i], source=src)
             all_requests.append(reqr)
 
         MPI.Request.Waitall(all_requests)
 
-        # Unpack code here!
+        # Unpack
+        numblocks_unpack = [
+            (ghost_size + (threadsperblock - 1)) // threadsperblock
+            for ghost_size in ghosts_size
+        ]
+        for i, rb in enumerate(recv_buff):
+            unpack_rev[numblocks_unpack[i], threadsperblock](rb, buffer, ghosts_idx[i])
 
     return scatter
+
+
+def scatter_forward(comm, owners_data, ghosts_data, N, float_type):
+    """
+    ADD DOC!
+    """
+
+    nlocal, nghost = N
+    owners_idx, owners_size, owners_offsets, owners = owners_data
+    ghosts_idx, ghosts_size, ghosts_offsets, ghosts = ghosts_data
+
+    send_buff = [cuda.device_array((ghost_size,), dtype=float_type) for ghost_size in ghosts_size]
+    recv_buff = [cuda.device_array((owner_size,), dtype=float_type) for owner_size in owners_size]
+    ghosts_buff = cuda.device_array((nghost, ), dtype=float_type)
+
+    def scatter(buffer):
+        """
+        ADD DOC!
+        """
+
+        all_requests = []
+
+        # Set the number of threads
+        threadsperblock = 128
+
+        # Pack
+        numblocks_pack = [
+            (ghost_size + (threadsperblock - 1)) // threadsperblock
+            for ghost_size in ghosts_size
+        ]
+        for i, sb in enumerate(send_buff):
+            pack[numblocks_pack[i], threadsperblock](buffer, sb, ghosts_idx[i])
+
+        
+
--- cuda/test_scatterer.py
+++ cuda/test_scatterer.py
@@ -13,9 +13,7 @@ from dolfinx.fem import functionspace, Function
 from dolfinx.la import InsertMode
 from dolfinx.mesh import create_box, CellType, GhostMode
 
-from precompute import compute_scaled_jacobian_determinant
-from operators import mass_operator
-from scatterer import scatter_reverse
+from scatterer import scatter_reverse, scatter_forward
 
 # MPI
 comm = MPI.COMM_WORLD
@@ -26,7 +24,7 @@ if cuda.is_available():
     print("CUDA is available")
 
 cuda.detect()
-# cuda.select_device(rank)
+cuda.select_device(rank)
 
 print(f"{rank} : {cuda.get_current_device()}")
 
@@ -46,7 +44,7 @@ Q = {
     10: 18,
 }  # Quadrature degree
 
-N = 4
+N = 32
 mesh = create_box(
     comm, ((0., 0., 0.), (1., 1., 1.)),
     (N, N, N), cell_type=CellType.hexahedron, 
@@ -80,11 +78,18 @@ nlocal = imap.size_local
 nghost = imap.num_ghosts
 owners = imap.owners
 unique_owners, owners_size = np.unique(owners, return_counts=True)
-owners_idx = np.argsort(owners)
-owners_idx_d = cuda.to_device(owners_idx)
+owners_argsorted = np.argsort(owners)
 
 owners_offsets = np.cumsum(owners_size)
 owners_offsets = np.insert(owners_offsets, 0, 0)
+
+owners_idx = [np.zeros(size, dtype=np.int64) for size in owners_size]
+for i, owner in enumerate(unique_owners):
+    begin = owners_offsets[i]
+    end = owners_offsets[i + 1]
+    owners_idx[i] = owners_argsorted[begin:end]
+
+owners_idx_d = [cuda.to_device(owner_idx) for owner_idx in owners_idx]
 
 # Compute owned data by this process that are ghosts data in other process 
 shared_dofs = imap.index_to_dest_ranks()
@@ -103,52 +108,36 @@ ghosts_offsets = np.insert(ghosts_offsets, 0, 0)
 
 all_requests = []
 
-@cuda.jit
-def copy_range(in_, out_, begin, end):
-    thread_id = cuda.threadIdx.x
-    block_id = cuda.blockIdx.x
-    idx = thread_id + block_id * cuda.blockDim.x
-
-    if idx < end - begin:
-        out_[idx] = in_[idx + begin]
-    
-
 # Send
-# send_buff_idx = np.zeros(np.sum(owners_size), dtype=np.int64)
-# send_buff_idx[:] = imap.ghosts[owners_idx]
 send_buff_idx = [np.zeros(size, dtype=np.int64) for size in owners_size]
 for i, owner in enumerate(unique_owners):
     begin = owners_offsets[i]
     end = owners_offsets[i + 1]
-    send_buff_idx[i] = imap.ghosts[owners_idx[begin:end]]
+    send_buff_idx[i] = imap.ghosts[owners_argsorted[begin:end]]
 
 send_buff_idx_d = [cuda.to_device(send_buff) for send_buff in send_buff_idx]
 for i, owner in enumerate(unique_owners):
-    # reqs = comm.Isend(send_buff_idx_d[i], dest=owner)
-    print(type(send_buff_idx_d[i]))
-    # all_requests.append(reqs)
+    reqs = comm.Isend(send_buff_idx_d[i], dest=owner)
+    all_requests.append(reqs)
 
 # Receive
-recv_buff_idx = np.zeros(np.sum(ghosts_size), dtype=np.int64)
-recv_buff_idx_d = cuda.to_device(recv_buff_idx)
+recv_buff_idx = [np.zeros(size, dtype=np.int64) for size in ghosts_size]
+recv_buff_idx_d = [cuda.to_device(recv_buff) for recv_buff in recv_buff_idx]
 for i, ghost in enumerate(unique_ghosts):
-    begin = ghosts_offsets[i]
-    end = ghosts_offsets[i + 1]
-    reqr = comm.Irecv(recv_buff_idx_d[begin:end], source=ghost)
+    reqr = comm.Irecv(recv_buff_idx_d[i], source=ghost)
     all_requests.append(reqr)
 
 MPI.Request.Waitall(all_requests)
 
-recv_buff_idx = recv_buff_idx_d.copy_to_host()
+for i, ghosts in enumerate(unique_ghosts):
+    recv_buff_idx[i] = recv_buff_idx_d[i].copy_to_host()
 
-ghosts_idx = recv_buff_idx - imap.local_range[0]
-ghosts_idx_d = cuda.to_device(ghosts_idx)
+ghosts_idx = [recv_buff - imap.local_range[0] for recv_buff in recv_buff_idx]
+
+ghosts_idx_d = [cuda.to_device(ghost_buff) for ghost_buff in ghosts_idx]
 
 owners_data_d = [owners_idx_d, owners_size, owners_offsets, unique_owners]
 ghosts_data_d = [ghosts_idx_d, ghosts_size, ghosts_offsets, unique_ghosts]
-
-print(f"{rank}: {owners_size}")
-exit()
 
 # Define function for testing
 u0 = Function(V, dtype=float_type)
@@ -161,8 +150,29 @@ u_ = u0.x.array.copy()
 # -------------------- #
 
 scatter_rev = scatter_reverse(
-    comm, owners_data_d, ghosts_data_d, nlocal, float_type
+    comm, owners_data_d, ghosts_data_d, [nlocal, nghost], float_type
 )
 
 # Allocate memory on the device
 u_d = cuda.to_device(u_)
+
+# Scatter
+scatter_rev(u_d)
+
+# Copy to host
+u_d.copy_to_host(u_)
+
+# Do scatter reverse using DOLFINx
+u0.x.scatter_reverse(InsertMode.add)
+
+# Check the difference between the vectors
+print(f"REVERSE: {rank}: {np.allclose(u0.x.array, u_)}", flush=True)
+
+# -------------------- #
+# Test scatter forward #
+# -------------------- #
+
+# scatter_fwd = scatter_forward(
+#     comm, owners_data_d, ghosts_data_d, [nlocal, nghost], float_type
+# )
+
