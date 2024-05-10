3cdaab5f759b042ba3137cbc43b2ebc938ca1d16
Adeeb Arif Kor <mabm4@cam.ac.uk>
2024-05-10 12:43:46 +0800

    Add scatterer to enable multi-GPU test

--- cuda/test_operators.py
+++ cuda/test_operators.py
@@ -14,6 +14,7 @@ import numba.cuda as cuda
 import basix
 import basix.ufl
 from dolfinx.fem import assemble_vector, functionspace, form, Function
+from dolfinx.la import InsertMode
 from dolfinx.mesh import create_box, locate_entities_boundary, CellType, GhostMode
 from ufl import inner, grad, ds, dx, TestFunction
 
@@ -23,7 +24,12 @@ from precompute import (
     compute_boundary_facets_scaled_jacobian_determinant,
 )
 from operators import mass_operator, stiffness_operator
+from scatterer import scatter_reverse, scatter_forward
 from utils import facet_integration_domain
+
+# MPI
+comm = MPI.COMM_WORLD
+rank = comm.rank
 
 # Check if CUDA is available
 if cuda.is_available():
@@ -51,9 +57,9 @@ Q = {
     10: 18,
 }  # Quadrature degree
 
-N = 8
+N = 4
 mesh = create_box(
-    MPI.COMM_WORLD,
+    comm,
     ((0.0, 0.0, 0.0), (1.0, 1.0, 1.0)),
     (N, N, N),
     cell_type=CellType.hexahedron,
@@ -74,12 +80,90 @@ family = basix.ElementFamily.P
 variant = basix.LagrangeVariant.gll_warped
 cell_type = mesh.basix_cell()
 
-basix_element = basix.create_tp_element(family, cell_type, P, variant)
+basix_element = basix.create_tp_element(family, cell_type, P, variant, dtype=float_type)
 element = basix.ufl._BasixElement(basix_element)  # basix ufl element
 
 # Create function space
 V = functionspace(mesh, element)
 dofmap = V.dofmap.list
+imap = V.dofmap.index_map
+
+if rank == 0:
+    print(f"Number of degrees-of-freedom: {imap.size_global}")
+
+# ------------ #
+# Scatter data #
+# ------------ #
+
+# Compute ghosts data in this process that are owned by other processes
+nlocal = imap.size_local
+nghost = imap.num_ghosts
+owners = imap.owners
+unique_owners, owners_size = np.unique(owners, return_counts=True)
+owners_argsorted = np.argsort(owners)
+
+owners_offsets = np.cumsum(owners_size)
+owners_offsets = np.insert(owners_offsets, 0, 0)
+
+owners_idx = [np.zeros(size, dtype=np.int64) for size in owners_size]
+for i, owner in enumerate(unique_owners):
+    begin = owners_offsets[i]
+    end = owners_offsets[i + 1]
+    owners_idx[i] = owners_argsorted[begin:end]
+
+owners_idx_d = [cuda.to_device(owner_idx) for owner_idx in owners_idx]
+
+# Compute owned data by this process that are ghosts data in other process
+shared_dofs = imap.index_to_dest_ranks()
+shared_ranks = np.unique(shared_dofs.array)
+
+ghosts = []
+for shared_rank in shared_ranks:
+    for dof in range(nlocal):
+        if shared_rank in shared_dofs.links(dof):
+            ghosts.append(shared_rank)
+
+ghosts = np.array(ghosts)
+unique_ghosts, ghosts_size = np.unique(ghosts, return_counts=True)
+ghosts_offsets = np.cumsum(ghosts_size)
+ghosts_offsets = np.insert(ghosts_offsets, 0, 0)
+
+all_requests = []
+
+# Send
+send_buff_idx = [np.zeros(size, dtype=np.int64) for size in owners_size]
+for i, owner in enumerate(unique_owners):
+    begin = owners_offsets[i]
+    end = owners_offsets[i + 1]
+    send_buff_idx[i] = imap.ghosts[owners_argsorted[begin:end]]
+
+send_buff_idx_d = [cuda.to_device(send_buff) for send_buff in send_buff_idx]
+for i, owner in enumerate(unique_owners):
+    reqs = comm.Isend(send_buff_idx_d[i], dest=owner)
+    all_requests.append(reqs)
+
+# Receive
+recv_buff_idx = [np.zeros(size, dtype=np.int64) for size in ghosts_size]
+recv_buff_idx_d = [cuda.to_device(recv_buff) for recv_buff in recv_buff_idx]
+for i, ghost in enumerate(unique_ghosts):
+    reqr = comm.Irecv(recv_buff_idx_d[i], source=ghost)
+    all_requests.append(reqr)
+
+MPI.Request.Waitall(all_requests)
+
+for i, ghosts in enumerate(unique_ghosts):
+    recv_buff_idx[i] = recv_buff_idx_d[i].copy_to_host()
+
+ghosts_idx = [recv_buff - imap.local_range[0] for recv_buff in recv_buff_idx]
+
+ghosts_idx_d = [cuda.to_device(ghost_buff) for ghost_buff in ghosts_idx]
+
+owners_data_d = [owners_idx_d, owners_size, unique_owners]
+ghosts_data_d = [ghosts_idx_d, ghosts_size, unique_ghosts]
+
+# Instantiate scatterer
+scatter_rev = scatter_reverse(comm, owners_data_d, ghosts_data_d, nlocal, float_type)
+scatter_fwd = scatter_forward(comm, owners_data_d, ghosts_data_d, nlocal, float_type)
 
 # Create function
 u0 = Function(V, dtype=float_type)  # Input function
@@ -194,12 +278,15 @@ mass_operator[num_blocks_c, threadsperblock_c](
     u_d, cell_constants_d, b_d, detJ_d, dofmap_d
 )
 
+# Perform scatter reverse
+scatter_rev(b_d)
+
 # Copy the result back to the host
 b_d.copy_to_host(b)
 
 a0_dolfinx = form(inner(u0, v) * dx(metadata=md), dtype=float_type)
 b0_dolfinx = assemble_vector(a0_dolfinx)
-# b0_dolfinx.scatter_reverse(InsertMode.add)
+b0_dolfinx.scatter_reverse(InsertMode.add)
 
 # Check the difference between the vectors
 mass_difference = np.linalg.norm(b - b0_dolfinx.array) / np.linalg.norm(
@@ -255,12 +342,15 @@ stiff_operator_cell[num_blocks, threadsperblock](
     u_d, cell_constants_d, b_d, G_d, dofmap_d, dphi_1D_d
 )
 
+# Perform scatter reverse
+scatter_rev(b_d)
+
 # Copy the result back to the host
 b_d.copy_to_host(b)
 
 a1_dolfinx = form(inner(grad(u0), grad(v)) * dx(metadata=md), dtype=float_type)
 b1_dolfinx = assemble_vector(a1_dolfinx)
-# b1_dolfinx.scatter_reverse(InsertMode.add)
+b1_dolfinx.scatter_reverse(InsertMode.add)
 
 # Check the difference between the vectors
 stiffness_difference = np.linalg.norm(b - b1_dolfinx.array) / np.linalg.norm(
@@ -295,12 +385,15 @@ mass_operator[num_blocks_bfacet, threadsperblock_bfacet](
     u_d, bfacet_constants_d, b_d, detJ_f_d, bfacet_dofmap_d
 )
 
+# Perform scatter reverse
+scatter_rev(b_d)
+
 # Copy the result back to the host
 b_d.copy_to_host(b)
 
 a3_dolfinx = form(inner(u0, v) * ds(metadata=md), dtype=float_type)
 b3_dolfinx = assemble_vector(a3_dolfinx)
-# b3_dolfinx.scatter_reverse(InsertMode.add)
+b3_dolfinx.scatter_reverse(InsertMode.add)
 
 # Check the difference between the vectors
 bfacet_difference = np.linalg.norm(b - b3_dolfinx.array) / np.linalg.norm(
